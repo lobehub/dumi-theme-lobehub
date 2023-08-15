5b8dccfabb6c2fe1702b88ce539be8579ad7e02f
canisminor1990 <i@canisminor.cc>
2023-08-15 21:22:01 +0800

    ✨ feat: Add favicons and metadata to web page using Helmet component
    This commit introduces the addition of favicons and metadata to a web page
    using the Helmet component. It includes links to different favicon images, a
    manifest file, and a mask icon. It also sets the title and theme color of the
    web app.
    
    Changes made:
    - Added favicons and metadata using Helmet component
    - Included links to different favicon images
    - Added a manifest file and a mask icon
    - Set the title and theme color of the web app

--- src/components/Favicons/index.tsx
+++ src/components/Favicons/index.tsx
@@ -1,11 +1,39 @@
-import { Meta } from '@lobehub/ui';
 import { Helmet } from 'dumi';
 import { memo } from 'react';
 
 const Favicons = memo(() => {
   return (
     <Helmet>
-      <Meta title={'LobeUI'} withManifest />
+      <link
+        href="https://registry.npmmirror.com/@lobehub/assets-favicons/1.1.0/files/assets/apple-touch-icon.png"
+        rel="apple-touch-icon"
+        sizes="180x180"
+      />
+      <link
+        href="https://registry.npmmirror.com/@lobehub/assets-favicons/1.1.0/files/assets/favicon-32x32.png"
+        rel="icon"
+        sizes="32x32"
+        type="image/png"
+      />
+      <link
+        href="https://registry.npmmirror.com/@lobehub/assets-favicons/1.1.0/files/assets/favicon-16x16.png"
+        rel="icon"
+        sizes="16x16"
+        type="image/png"
+      />
+      <link
+        href="https://registry.npmmirror.com/@lobehub/assets-favicons/1.1.0/files/assets/site.webmanifest"
+        rel="manifest"
+      />
+      <link
+        color="#000000"
+        href="https://registry.npmmirror.com/@lobehub/assets-favicons/1.1.0/files/assets/safari-pinned-tab.svg"
+        rel="mask-icon"
+      />
+      <meta content="LobeHub" name="apple-mobile-web-app-title" />
+      <meta content="LobeHub" name="application-name" />
+      <meta content="#000000" name="msapplication-TileColor" />
+      <meta content="#000000" name="theme-color" />
     </Helmet>
   );
 });
