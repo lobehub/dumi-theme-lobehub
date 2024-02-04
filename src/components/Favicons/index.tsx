de95d6187ddef584f81fbd8f48b895e8f515f9f4
tnga <devtnga@gmail.com>
2024-02-04 18:50:57 +0100

    ♻️ refactor: Update Favicons component to use latest version of assets-favicons package
    This commit updates the Favicons component to use the latest version of the assets-favicons package.
    The links to the favicon files have been updated to point to the latest version of the package.
    This refactor does not add any new functionality or fix any bugs, but ensures that the component is using the most up-to-date assets.

--- src/components/Favicons/index.tsx
+++ src/components/Favicons/index.tsx
@@ -5,29 +5,29 @@ const Favicons = memo(() => {
   return (
     <Helmet>
       <link
-        href="https://registry.npmmirror.com/@lobehub/assets-favicons/1.1.0/files/assets/apple-touch-icon.png"
+        href="https://registry.npmmirror.com/@lobehub/assets-favicons/latest/files/assets/apple-touch-icon.png"
         rel="apple-touch-icon"
         sizes="180x180"
       />
       <link
-        href="https://registry.npmmirror.com/@lobehub/assets-favicons/1.1.0/files/assets/favicon-32x32.png"
+        href="https://registry.npmmirror.com/@lobehub/assets-favicons/latest/files/assets/favicon-32x32.png"
         rel="icon"
         sizes="32x32"
         type="image/png"
       />
       <link
-        href="https://registry.npmmirror.com/@lobehub/assets-favicons/1.1.0/files/assets/favicon-16x16.png"
+        href="https://registry.npmmirror.com/@lobehub/assets-favicons/latest/files/assets/favicon-16x16.png"
         rel="icon"
         sizes="16x16"
         type="image/png"
       />
       <link
-        href="https://registry.npmmirror.com/@lobehub/assets-favicons/1.1.0/files/assets/site.webmanifest"
+        href="https://registry.npmmirror.com/@lobehub/assets-favicons/latest/files/assets/site.webmanifest"
         rel="manifest"
       />
       <link
         color="#000000"
-        href="https://registry.npmmirror.com/@lobehub/assets-favicons/1.1.0/files/assets/safari-pinned-tab.svg"
+        href="https://registry.npmmirror.com/@lobehub/assets-favicons/latest/files/assets/safari-pinned-tab.svg"
         rel="mask-icon"
       />
       <meta content="LobeHub" name="apple-mobile-web-app-title" />
