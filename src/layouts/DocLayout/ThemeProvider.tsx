f234ba42873deaa9c5a53e5ca6e8985c3354eb78
canisminor1990 <i@canisminor.cc>
2025-08-18 23:38:56 +0800

    🐛 fix: Fix themmode

--- src/layouts/DocLayout/ThemeProvider.tsx
+++ src/layouts/DocLayout/ThemeProvider.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from '@lobehub/ui';
-import { StyleProvider, extractStaticStyle } from 'antd-style';
 import 'antd/dist/reset.css';
 import isEqual from 'fast-deep-equal';
 import { PropsWithChildren, memo } from 'react';
@@ -8,26 +7,21 @@ import GlobalStyle from '@/layouts/DocLayout/GlobalStyle';
 import { siteSelectors, useSiteStore, useThemeStore } from '@/store';
 import customToken from '@/styles/customToken';
 
-//@ts-ignore
-global.__ANTD_CACHE__ = extractStaticStyle.cache;
-
 export default memo<PropsWithChildren>(({ children }) => {
   const themeMode = useThemeStore((st) => st.themeMode);
   const userToken = useSiteStore(siteSelectors.token, isEqual);
 
   return (
-    <StyleProvider cache={extractStaticStyle.cache} speedy={process.env.NODE_ENV === 'production'}>
-      <ThemeProvider
-        appearance={themeMode !== 'auto' ? themeMode : undefined}
-        customToken={(themeToken) => Object.assign({}, customToken(themeToken), userToken)}
-        theme={{
-          cssVar: true,
-        }}
-        themeMode={themeMode}
-      >
-        <GlobalStyle />
-        {children}
-      </ThemeProvider>
-    </StyleProvider>
+    <ThemeProvider
+      appearance={themeMode !== 'auto' ? themeMode : undefined}
+      customToken={(themeToken) => Object.assign({}, customToken(themeToken), userToken)}
+      theme={{
+        cssVar: true,
+      }}
+      themeMode={themeMode}
+    >
+      <GlobalStyle />
+      {children}
+    </ThemeProvider>
   );
 });
