a4c42928893467e6809a392a902d3a9bb3435c6e
canisminor1990 <i@canisminor.cc>
2025-08-18 23:59:47 +0800

    💄 style: Fix __ANTD_CACHE__

--- src/layouts/DocLayout/StyleRegistry.tsx
+++ src/layouts/DocLayout/StyleRegistry.tsx
@@ -3,8 +3,15 @@
 import { StyleProvider, extractStaticStyle } from 'antd-style';
 import { PropsWithChildren } from 'react';
 
+// @ts-ignore
+global.__ANTD_CACHE__ = extractStaticStyle.cache;
+
 const StyleRegistry = ({ children }: PropsWithChildren) => {
-  return <StyleProvider cache={extractStaticStyle.cache}>{children}</StyleProvider>;
+  return (
+    <StyleProvider cache={extractStaticStyle.cache} speedy={process.env.NODE_ENV === 'production'}>
+      {children}
+    </StyleProvider>
+  );
 };
 
 export default StyleRegistry;
