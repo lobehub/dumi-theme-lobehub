3f7d15bb80bfcdd1bbe2ca690b3c3867ad0efbb1
canisminor1990 <i@canisminor.cc>
2025-12-20 21:36:32 +0800

    🐛 fix: Fix background

--- src/layouts/DemoLayout/GlobalStyle.ts
+++ src/layouts/DemoLayout/GlobalStyle.ts
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'antd-style';
 
 const GlobalStyle = createGlobalStyle`
+  html, body {
+    background: ${({ theme }) => theme.colorBgContainerSecondary};
+  }
+
   #root {
     min-height: 100vh;
     background: ${({ theme }) => theme.colorBgContainerSecondary};
--- src/layouts/DocLayout/GlobalStyle.ts
+++ src/layouts/DocLayout/GlobalStyle.ts
@@ -1,8 +1,13 @@
 import { createGlobalStyle } from 'antd-style';
 
 const GlobalStyle = createGlobalStyle`
+  html, body {
+    background: ${({ theme }) => theme.colorBgContainerSecondary};
+  }
+
   #root {
     min-height: 100vh;
+    background: ${({ theme }) => theme.colorBgContainerSecondary};
   }
 
   #nprogress {
