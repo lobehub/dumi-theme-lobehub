75273a1aad82902284f4a0558c3a424ef16abe09
Innei <tukon479@gmail.com>
2026-01-06 19:48:03 +0800

    🔧 chore: Update @lobehub/ui to v4.11.0 and enhance ThemeSwitch component
    - Upgraded @lobehub/ui from v4.3.3 to v4.11.0 in package.json.
    - Refactored ThemeSwitch component to improve theme switching functionality with transition handling and state management.
    
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/slots/Header/ThemeSwitch.tsx
+++ src/slots/Header/ThemeSwitch.tsx
@@ -1,19 +1,58 @@
 import { ThemeSwitch as ThemeSwitchButton } from '@lobehub/ui';
+import type { ThemeMode } from 'antd-style';
 import { usePrefersColor } from 'dumi';
-import { memo, useEffect } from 'react';
+import { memo, useCallback, useEffect, useRef, useState } from 'react';
 
 import { useThemeStore } from '@/store/useThemeStore';
+
+const DISABLE_TRANSITION_STYLE =
+  '*,*::before,*::after{-webkit-transition:none!important;-moz-transition:none!important;-o-transition:none!important;-ms-transition:none!important;transition:none!important}';
 
 const ThemeSwitch = memo(() => {
   const themeMode = useThemeStore((s) => s.themeMode);
   const setColorMode = usePrefersColor()[2];
   useEffect(() => setColorMode(themeMode), [themeMode]);
 
+  const styleRef = useRef<HTMLStyleElement | null>(null);
+  const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const handleThemeSwitch = useCallback((mode: ThemeMode) => {
+    const transitionDisableDuration = 100;
+
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+
+    if (styleRef.current) {
+      styleRef.current.remove();
+    }
+
+    const css = document.createElement('style');
+    css.append(document.createTextNode(DISABLE_TRANSITION_STYLE));
+    document.head.append(css);
+    styleRef.current = css;
+
+    useThemeStore.setState({ themeMode: mode });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    window.getComputedStyle(document.body).opacity;
+
+    timerRef.current = setTimeout(() => {
+      if (styleRef.current) {
+        styleRef.current.remove();
+        styleRef.current = null;
+      }
+    }, transitionDisableDuration);
+  }, []);
+
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
   return (
     <ThemeSwitchButton
-      onThemeSwitch={(themeMode) => {
-        useThemeStore.setState({ themeMode });
-      }}
+      key={isMounted ? 'mounted' : 'unmounted'}
+      onThemeSwitch={handleThemeSwitch}
       themeMode={themeMode}
     />
   );
